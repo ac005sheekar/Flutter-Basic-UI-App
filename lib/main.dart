2538d2a2a3f176d9a40f6fd73276a01b3d07fd89
Sheekar Banerjee <32336082+ac005sheekar@users.noreply.github.com>
2022-07-03 16:18:53 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Sheekar Banerjee - World Time App "),
-      centerTitle: true,
-      backgroundColor: Colors.red[400],
-    ),
-    body: Center(
-      child: Text(
+  home: Home(),
+));
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sheekar Banerjee - World Time App "),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Center(
+        child: Text(
           'Hello Time Trackers!',
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey[600],
-            
-          ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child: Text('click'),
-      backgroundColor: Colors.red[400],
-    ),
 
-  ),
-));
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('click'),
+        backgroundColor: Colors.red[400],
+      ),
+
+
+    );
+  }
+}
