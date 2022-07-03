02215b27841f7d7bc37706028e48cf31392e8b08
Sheekar Banerjee <32336082+ac005sheekar@users.noreply.github.com>
2022-07-03 16:07:34 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,16 @@ void main() => runApp(MaterialApp(
       backgroundColor: Colors.red[400],
     ),
     body: Center(
-      child: Text('Hello Time Trackers!'),
+      child: Text(
+          'Hello Time Trackers!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            
+          ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: null,
